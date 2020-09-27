a55a78d393cf9839cd2ca9ad04063b8d9330b6e3
Mike Duffy <mike.duffy@parivedasolutions.com>
2020-09-27 12:13:31 -0500

    Fixed bug in search.query_with_paging where an additional final empty page was being returned by the generator. Needed to conver the the while-loop to a do-while condition.

--- osdu/search.py
+++ osdu/search.py
@@ -12,7 +12,7 @@ class SearchService(BaseService):
 
 
     def query(self, query: dict) -> dict:
-        """
+        """Executes a query against the OSDU search service.
 
         :param query:   dict representing the JSON-style query to be sent to the search API. Must adhere to
                         the Lucene syntax suported by OSDU. For more details, see: 
@@ -33,20 +33,37 @@ class SearchService(BaseService):
 
 
     def query_with_paging(self, query: dict):
+        """Executes a query with cursor against the OSDU search service. Returns a generator, which can than be
+        iterated over to retrieve each page in the result set without having to deal with any cursor.
+
+        :param query:   dict representing the JSON-style query to be sent to the search API. Must adhere to
+                        the Lucene syntax suported by OSDU. For more details, see: 
+                        https://community.opengroup.org/osdu/documentation/-/wikis/Releases/R2.0/OSDU-Query-Syntax
+
+        :returns:       iterator containing  items: results, totalCount
+                        - results:      list:   one page of records resutling from search query. Default page size
+                                                is 10. This can be modified by passing the 'limit' parameter in
+                                                query with the maximum allowed being 1000.
+                        - totalCount:   int:    the total number of results despite any 'limit' specified in the
+                                                query or the 1,000 record limit of the API
+        """
         url = f'{self._service_url}/query_with_cursor'
-        cursor='initial'
+        cursor=''
 
         try:
             while cursor:
                 # Add cursor to request body for subsequent requests.
-                if cursor != "initial":
-                    query["cursor"] = cursor
+                if cursor != '':
+                    query['cursor'] = cursor
                 
                 response = requests.post(url=url, headers=self._headers(), json=query)
                 response.raise_for_status()
 
                 cursor, results, total_count  = response.json().values()
-                yield results, total_count
+                if not cursor:  # Effetive do-while condition
+                    break
+                else:
+                    yield results, total_count
 
         except requests.exceptions.HTTPError as e:
             logging.error(e)
