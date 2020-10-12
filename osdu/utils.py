db50418b2ecc80f04682e6e44b5bba7d0d43921b
Mike Duffy <mike.duffy@parivedasolutions.com>
2020-10-12 10:46:49 -0500

    Intial utils module

---
+++ osdu/utils.py
@@ -0,0 +1,11 @@
+import json
+
+
+def format_json(obj):
+    return json.dumps(obj, indent=2, sort_keys=True)
+
+
+def batches(lst, n):
+    """Yield successive n-sized batches from list."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
