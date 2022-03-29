48a313f3976eee8a9854d26c489d320c13797597
Johnny Reichman <88909002+JohnnyReichmanPariveda@users.noreply.github.com>
2022-03-29 14:32:44 -0500

    update comments

--- osdu/services/legal.py
+++ osdu/services/legal.py
@@ -11,35 +11,42 @@ class LegalService(BaseService):
         super().__init__(client, service_name='legal', service_version=1)
 
     def get_legaltag(self, legaltag_name: str):
-        """Returns information about the given legaltag."""
+        """Returns information about the given legaltag.
+        
+        param legaltag_name: the name of the legaltag of interest
+        """
         url = f'{self._service_url}/legaltags/{legaltag_name}'
         response = self.__execute_request('get', url)
 
         return response.json()
     
     def create_legaltag(self, legaltag: dict):
-        """Create a new legaltag. """
+        """Create a new legaltag. 
+        
+        param legaltag: a JSON representation of a legaltag 
+        """
         url = f'{self._service_url}/legaltags'
         response = self.__execute_request('post', url, json=legaltag)
 
         return response.json()
     
-    def delete_legaltag(self, legaltag_id: str) -> bool:
+    def delete_legaltag(self, legaltag_name: str) -> bool:
         """Deletes the given legaltag. This operation cannot be reverted (except by re-creating the legaltag).
         
+        :param legaltag_name:   the name of the legaltag to delete
         :returns:   True if legaltag deleted successfully. Otherwise False.
         """
-        url = f'{self._service_url}/legaltags/{legaltag_id}'
+        url = f'{self._service_url}/legaltags/{legaltag_name}'
         response = self.__execute_request('delete', url)
 
         return response.status_code == 204
 
-    def get_legaltags(self, isValid: bool = True):
+    def get_legaltags(self, valid: bool = True):
         """Fetches all matching legaltags.
 
         :param valid:  Boolean to restrict results to only valid legaltags (true) or only invalid legal tags (false). Default is true
         """
-        url = f'{self._service_url}/legaltags/' + ('?valid=true' if isValid else '?valid=false')
+        url = f'{self._service_url}/legaltags/' + ('?valid=true' if valid else '?valid=false')
         response = self.__execute_request('get', url)
 
         return response.json()
@@ -77,7 +84,7 @@ class LegalService(BaseService):
         return response.json()
 
     def get_legaltag_properties(self):
-        """Fetch information about possible valuesn for legaltag properties"""
+        """Fetch information about possible values for legaltag properties"""
         url = f'{self._service_url}/legaltags:properties'
         response = self.__execute_request('get', url)
 
